5a080cf71f2076066e31ba094bcccf3035320a6a
tahacmv <tmmotassimahmedtaha@gmail.com>
2025-02-19 20:50:21 +0100

    added resilience4j circuit breaker on api gateway service calls

--- api-gateway/src/main/java/com/amotassim/api_gateway/configuration/GatewayConfig.java
+++ api-gateway/src/main/java/com/amotassim/api_gateway/configuration/GatewayConfig.java
@@ -8,35 +8,68 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GatewayConfig {
 
-    @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route("patient_service", r -> r.path("/patients/**")
-                        .uri("lb://PATIENT-SERVICE"))
-                .route("patient_by_id", r -> r.path("/patients/{id}")
-                        .uri("lb://PATIENT-SERVICE"))
-                .route("add_patient", r -> r.path("/patients")
-                        .and().method("POST")
-                        .uri("lb://PATIENT-SERVICE"))
-                .route("update_patient", r -> r.path("/patients/{id}")
-                        .and().method("PUT")
-                        .uri("lb://PATIENT-SERVICE"))
-                .route("delete_patient", r -> r.path("/patients/{id}")
-                        .and().method("DELETE")
-                        .uri("lb://PATIENT-SERVICE"))
-                .route("practitioner_service", r -> r.path("/practitioner/**")
-                        .uri("lb://PRACTITIONER-SERVICE"))
-                .route("practitioner_by_id", r -> r.path("/practitioner/{id}")
-                        .uri("lb://PRACTITIONER-SERVICE"))
-                .route("add_practitioner", r -> r.path("/practitioner")
-                        .and().method("POST")
-                        .uri("lb://PRACTITIONER-SERVICE"))
-                .route("update_practitioner", r -> r.path("/practitioner/{id}")
-                        .and().method("PUT")
-                        .uri("lb://PRACTITIONER-SERVICE"))
-                .route("delete_practitioner", r -> r.path("/practitioner/{id}")
-                        .and().method("DELETE")
-                        .uri("lb://PRACTITIONER-SERVICE"))
-                .build();
-    }
+        @Bean
+        public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+                return builder.routes()
+                                // Patient Service Routes with Circuit Breaker and Rate Limiter
+                                .route("patient_service", r -> r.path("/patients/**")
+                                                .filters(f -> f
+                                                                .circuitBreaker(c -> c.setName("patientService")
+                                                                                .setFallbackUri("forward:/fallback/patients")))
+                                                .uri("lb://PATIENT-SERVICE"))
+                                .route("patient_by_id", r -> r.path("/patients/{id}")
+                                                .filters(f -> f
+                                                                .circuitBreaker(c -> c.setName("patientService")
+                                                                                .setFallbackUri("forward:/fallback/patients")))
+                                                .uri("lb://PATIENT-SERVICE"))
+                                .route("add_patient", r -> r.path("/patients")
+                                                .and().method("POST")
+                                                .filters(f -> f
+                                                                .circuitBreaker(c -> c.setName("patientService")
+                                                                                .setFallbackUri("forward:/fallback/patients")))
+                                                .uri("lb://PATIENT-SERVICE"))
+                                .route("update_patient", r -> r.path("/patients/{id}")
+                                                .and().method("PUT")
+                                                .filters(f -> f
+                                                                .circuitBreaker(c -> c.setName("patientService")
+                                                                                .setFallbackUri("forward:/fallback/patients")))
+                                                .uri("lb://PATIENT-SERVICE"))
+                                .route("delete_patient", r -> r.path("/patients/{id}")
+                                                .and().method("DELETE")
+                                                .filters(f -> f
+                                                                .circuitBreaker(c -> c.setName("patientService")
+                                                                                .setFallbackUri("forward:/fallback/patients")))
+                                                .uri("lb://PATIENT-SERVICE"))
+
+                                // Practitioner Service Routes with Circuit Breaker and Rate Limiter
+                                .route("practitioner_service", r -> r.path("/practitioner/**")
+                                                .filters(f -> f
+                                                                .circuitBreaker(c -> c.setName("practitionerService")
+                                                                                .setFallbackUri("forward:/fallback/practitioners")))
+                                                .uri("lb://PRACTITIONER-SERVICE"))
+                                .route("practitioner_by_id", r -> r.path("/practitioner/{id}")
+                                                .filters(f -> f
+                                                                .circuitBreaker(c -> c.setName("practitionerService")
+                                                                                .setFallbackUri("forward:/fallback/practitioners")))
+                                                .uri("lb://PRACTITIONER-SERVICE"))
+                                .route("add_practitioner", r -> r.path("/practitioner")
+                                                .and().method("POST")
+                                                .filters(f -> f
+                                                                .circuitBreaker(c -> c.setName("practitionerService")
+                                                                                .setFallbackUri("forward:/fallback/practitioners")))
+                                                .uri("lb://PRACTITIONER-SERVICE"))
+                                .route("update_practitioner", r -> r.path("/practitioner/{id}")
+                                                .and().method("PUT")
+                                                .filters(f -> f
+                                                                .circuitBreaker(c -> c.setName("practitionerService")
+                                                                                .setFallbackUri("forward:/fallback/practitioners")))
+                                                .uri("lb://PRACTITIONER-SERVICE"))
+                                .route("delete_practitioner", r -> r.path("/practitioner/{id}")
+                                                .and().method("DELETE")
+                                                .filters(f -> f
+                                                                .circuitBreaker(c -> c.setName("practitionerService")
+                                                                                .setFallbackUri("forward:/fallback/practitioners")))
+                                                .uri("lb://PRACTITIONER-SERVICE"))
+                                .build();
+        }
 }
--- api-gateway/src/main/java/com/amotassim/api_gateway/controler/FallbackController.java
+++ api-gateway/src/main/java/com/amotassim/api_gateway/controler/FallbackController.java
@@ -1,20 +1,24 @@
 package com.amotassim.api_gateway.controler;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.http.HttpStatus;
 
 @RestController
 @RequestMapping("/fallback")
 public class FallbackController {
 
-    @GetMapping("/patientService")
-    public String patientServiceFallback() {
-        return "Patient Service is currently unavailable. Please try again later.";
-    }
-
-    @GetMapping("/practitionerService")
-    public String practitionerServiceFallback() {
-        return "Practitioner Service is currently unavailable. Please try again later.";
+    @GetMapping(value = "/{segment}")
+    public ResponseEntity<Object> fallback(@PathVariable String segment) {
+        Map<String, String> mapResponse = new HashMap<>();
+        mapResponse.put("status", "SERVICE " + StringUtils.upperCase(segment) + " IS UNAVAILABLE");
+        return new ResponseEntity<>(mapResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
